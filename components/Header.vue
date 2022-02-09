ab6d87570c789e491d824b110208a0f52f1d1cee
lfreyes97 <reyesdelosreyes.1997@gmail.com>
2022-02-08 19:03:55 -0500

    Navbar update

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,47 @@
 <template>
-<header class="h-24 sm:h-32 flex items-center z-30 w-full">
-        <div class="container mx-auto px-6 flex items-center justify-between">
-            <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
-                <h1>
-                Prof. Juan C. Reyes
-                </h1>
-            </div>
-            <div class="flex items-center">
-                <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
-                    <nuxt-link to="/" class="py-2 px-6 flex hover:text-blue-700">Inicio</nuxt-link>
-                    <nuxt-link to="/blog" class="py-2 px-6 flex hover:text-blue-700">Blog</nuxt-link>
-                    <nuxt-link to="/libros" class="py-2 px-6 flex hover:text-blue-700">Libros</nuxt-link>
-                    <nuxt-link to="/materiales" class="py-2 px-6 flex hover:text-blue-700">Materiales</nuxt-link>
-                    <nuxt-link to="/actividades" class="py-2 px-6 flex hover:text-blue-700">Actividades</nuxt-link>
-                </nav>
-            </div>
-        </div>
-    </header>
+
+<header class="shadow-sm">
+  <div class="max-w-screen-xl p-4 mx-auto">
+    <div class="flex items-center justify-between space-x-4 lg:space-x-10">
+      <div class="flex lg:w-0 lg:flex-1">
+        <span class="w-20 h-10 bg-gray-200 rounded-lg">
+          <h3>
+            Prof. Juan C. Reyes
+          </h3>
+        </span>
+      </div>
+
+      <nav class="hidden space-x-8 text-sm font-medium md:flex">
+          <nuxt-link to="/" class="py-2 px-6 flex hover:text-blue-700">Inicio</nuxt-link>
+          <nuxt-link to="/blog" class="py-2 px-6 flex hover:text-blue-700">Blog</nuxt-link>
+          <nuxt-link to="/libros" class="py-2 px-6 flex hover:text-blue-700">Libros</nuxt-link>
+          <nuxt-link to="/materiales" class="py-2 px-6 flex hover:text-blue-700">Materiales</nuxt-link>
+          <nuxt-link to="/actividades" class="py-2 px-6 flex hover:text-blue-700">Actividades</nuxt-link>
+      </nav>
+
+      <div class="items-center justify-end flex-1 hidden space-x-4 sm:flex">
+        <a class="px-5 py-2 text-sm font-medium text-gray-500 bg-gray-100 rounded-lg" href=""> Log in </a>
+
+        <a class="px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg" href=""> Sign up </a>
+      </div>
+
+      <div class="lg:hidden">
+        <button class="p-2 text-gray-600 bg-gray-100 rounded-lg" type="button">
+          <span class="sr-only">Open menu</span>
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewbox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</header>
+
 </template>
